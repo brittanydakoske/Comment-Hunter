af75ae0109127da89b96183a17d06b4f8a208a4d
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-04-09 14:53:33 -0700

    feature/config front-end test data
    [COM-144] Configure test data for front-end development
    - Created a new script "npm run serve" to start dev server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {getData} from "./api/request";
+import {useEffect, useState} from "react";
 
 function App() {
+  /* On page load, get data from server and store it in "data"
+  * You will notice that console.log( data ) returns an empty array at first
+  * and then returns the data. This is because both getData and setData are
+  * working asynchronously. */
+  const [data, setData] = useState([])
+  useEffect(() => {
+    getData().then( res => setData( res.data ) )
+  }, []);
+  console.log( data )
+
   return (
     <div className="App">
       <header className="App-header">
