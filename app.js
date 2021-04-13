a4cae3fd1d277c1fd98bc093056120a1c43f6e9f
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-04-13 11:13:50 -0700

    configure CORS
    Configure CORS middleware with express

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const helmet = require('helmet')
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 
@@ -14,6 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({origin: 'http://localhost:3000'}))
 
 app.use('/', indexRouter);
 
