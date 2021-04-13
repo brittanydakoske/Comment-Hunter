c1601fb8a13a3de3615faf0ccbe42cf66498f0ba
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-04-13 11:12:50 -0700

    Update index.js
    [COM-145] Build initial development controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,89 @@
-const models = require('../database/models');
+const sampleData =
+    [
+        {
+            "ticker":"ETSY",
+            "name":"Etsy",
+            "sector":"Commercial Services",
+            "values":"321"
+        },
+        {
+            "ticker":"VZ",
+            "name":"Verizon",
+            "sector":"Communications",
+            "values":"124"
+        },
+        {
+            "ticker":"APPL",
+            "name":"Amazon",
+            "sector":"Electronic Technology",
+            "values":"25"
+        },
+        {
+            "ticker":"",
+            "name":"AT&T",
+            "sector":"Communications",
+            "values":"221"
+        },
+        {
+            "ticker":"GME",
+            "name":"GameStop",
+            "sector":"Retail",
+            "values":"420"
+        },
+        {
+            "ticker":"TSLA",
+            "name":"Tesla",
+            "sector":"Consumer Durables",
+            "values":"123"
+        },
+        {
+            "ticker":"MRNA",
+            "name":"Moderna",
+            "sector":"Health Technology",
+            "values":"154"
+        },
+        {
+            "ticker":"COKE",
+            "name":"Coca-Cola",
+            "sector":"Consumer non-durables",
+            "values":"231"
+        },
+        {
+            "ticker":"BAC",
+            "name":"Bank of America",
+            "sector":"Finance",
+            "values":"157"
+        },
+        {
+            "ticker":"FDX",
+            "name":"FedEx",
+            "sector":"Transportation",
+            "values":"207"
+        }
+    ]
 
-module.exports = {}
+const get1h = async (req, res) => {
+    try {
+        return res.status(200).json(sampleData);
+    } catch (e) {
+        return res.status(500)
+    }
+}
+
+const get1d = async (req, res) => {
+    try {
+        return res.status(200).json(sampleData);
+    } catch (e) {
+        return res.status(500)
+    }
+}
+
+const get1w = async (req, res) => {
+    try {
+        return res.status(200).json(sampleData);
+    } catch (e) {
+        return res.status(500)
+    }
+}
+
+module.exports = { get1h, get1d, get1w }
