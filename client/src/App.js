b403bc11dd314401dc5864fc9d112aa69495c738
Luis Rangel <luismrangel7@gmail.com>
2021-04-13 19:23:13 -0700

    Initialized React App [COM-148]

--- client/src/App.js
+++ client/src/App.js
@@ -9,69 +9,29 @@ import {bubbleData} from  './components/data2';
 import Navbar from './components/Navbar';
 import './styles/App.css'; //styles/App.css ok
 
-// function App() {
+function App() {
 
-//   onSelect((event) => {
-//     //console.log(event);
-//   }, );
-
-//   reDraw = bubbleData.map(v => ({
-//     v: v.values,
-//     k: v.keys
-//   }));
-
-//   useEffect(() => {
-//     get1hr().then( res => console.log( res.data ) )
-//     get1day().then( res => console.log( res.data ) )
-//     get1week().then( res => console.log( res.data ) )
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>Comment Hunter</h1>
-//       <div className="graph-container">
-//           <BubbleChart data={this.reDraw} useLabels />
-//           {/* <Sunburst
-//           data={SunburstData}
-//           onSelect={this.onSelect}
-//           scale="linear" // or exponential
-//           tooltipContent={<div class="sunburstTooltip"  />} // eslint-disable-line
-//           tooltip
-//           tooltipPosition="right"
-//           keyId="anagraph"
-//           width="580"
-//           height="500"
-//         /> */}
-//         </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-//This class needs to be convertet to a function or hook instead
-//reDraw is giving issues if using hooks. Just needs to be rewritten.
-
-class App extends Component {
-  // onSelect(event){
+  // onSelect((event) => {
   //   //console.log(event);
-  // }
+  // }, );
 
-  reDraw = bubbleData.map(v => ({
+  var reDraw = bubbleData.map(v => ({
     v: v.values,
     k: v.keys
   }));
 
+  useEffect(() => {
+    get1hr().then( res => console.log( res.data ) )
+    get1day().then( res => console.log( res.data ) )
+    get1week().then( res => console.log( res.data ) )
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        {/* <Navbar /> */}
-        <h1>Hello from React</h1>
-        
-        <div className="graph-container">
-          <BubbleChart data={this.reDraw} useLabels />
+  return (
+    <div className="App">
+      <h1>Comment Hunter</h1>
+      
+      <div className="graph-container">
+          <BubbleChart data={reDraw} useLabels />
           {/* <Sunburst
           data={SunburstData}
           onSelect={this.onSelect}
@@ -84,10 +44,10 @@ class App extends Component {
           height="500"
         /> */}
         </div>
-        
-      </div>
-    );
-  }
+      
+
+    </div>
+  );
 }
 
 export default App;
