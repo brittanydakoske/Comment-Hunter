5c1f30a255fd44ba49e24f85c6bab547ba768f04
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-04-18 18:47:22 -0700

    Update index.js
    [COM-146]

--- controllers/index.js
+++ controllers/index.js
@@ -1,97 +1,62 @@
+const moment = require('moment')
 const models = require("../database/models");
-const sampleData =
-    [
-        {
-            "ticker":"ETSY",
-            "name":"Etsy",
-            "sector":"Commercial Services",
-            "values":"321"
-        },
-        {
-            "ticker":"VZ",
-            "name":"Verizon",
-            "sector":"Communications",
-            "values":"124"
-        },
-        {
-            "ticker":"APPL",
-            "name":"Amazon",
-            "sector":"Electronic Technology",
-            "values":"25"
-        },
-        {
-            "ticker":"",
-            "name":"AT&T",
-            "sector":"Communications",
-            "values":"221"
-        },
-        {
-            "ticker":"GME",
-            "name":"GameStop",
-            "sector":"Retail",
-            "values":"420"
-        },
-        {
-            "ticker":"TSLA",
-            "name":"Tesla",
-            "sector":"Consumer Durables",
-            "values":"123"
-        },
-        {
-            "ticker":"MRNA",
-            "name":"Moderna",
-            "sector":"Health Technology",
-            "values":"154"
-        },
-        {
-            "ticker":"COKE",
-            "name":"Coca-Cola",
-            "sector":"Consumer non-durables",
-            "values":"231"
-        },
-        {
-            "ticker":"BAC",
-            "name":"Bank of America",
-            "sector":"Finance",
-            "values":"157"
-        },
-        {
-            "ticker":"FDX",
-            "name":"FedEx",
-            "sector":"Transportation",
-            "values":"207"
-        }
-    ]
+const { Op } = require('sequelize')
+
+const filterStocks = ( stocks ) => {
+    let set = []
+    stocks.forEach( s => {
+        let index = set.findIndex( elem => elem.name === s.name)
+        index !== -1 ? set[index].value += 1 : set.push(
+            {
+                ticker: s.ticker,
+                name: s.name,
+                sector: s.sector,
+                value: 1
+            })
+    })
+    return set.sort( (a, b) => b.value - a.value).slice(0, 20)
+}
 
 const get1h = async (req, res) => {
     try {
-        return res.status(200).json(sampleData);
-    } catch (e) {
-        return res.status(500)
+        const stocks = await models.Stock.findAll({
+            where: {
+                date: {
+                    [Op.gte]: moment().subtract(1, 'hours').toDate()
+                }
+            }
+        })
+        return res.status( 200 ).json( filterStocks( stocks ) );
+    } catch ( e ) {
+        return res.status( 500 )
     }
 }
 
 const get1d = async (req, res) => {
     try {
-        return res.status(200).json(sampleData);
-    } catch (e) {
-        return res.status(500)
+        const stocks = await models.Stock.findAll({
+            where: {
+                date: {
+                    [Op.gte]: moment().subtract(1, 'days').toDate()
+                }
+            }
+        })
+        return res.status( 200 ).json( filterStocks( stocks ) );
+    } catch ( e ) {
+        return res.status( 500 )
     }
 }
 
 const get1w = async (req, res) => {
     try {
-        return res.status(200).json(sampleData);
-    } catch (e) {
-        return res.status(500)
-    }
-}
-
-/* query all config and haiku data */
-const getAll = async (req, res) => {
-    try {
-        const stocks = await models.Stock.findAll();
-        return res.status( 200 ).json( { stocks } );
+        const stocks = await models.Stock.findAll({
+            where: {
+                date: {
+                    [Op.gte]: moment().subtract(1, 'weeks').toDate()
+                }
+            }
+        })
+        return res.status( 200 ).json( filterStocks( stocks ) );
     } catch ( e ) {
         return res.status( 500 )
     }
@@ -102,5 +67,5 @@ module.exports =
         get1h,
         get1d,
         get1w,
-        getAll
     }
+
