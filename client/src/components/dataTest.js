c2b9dce2d258b6031718328fa8fa275b293ac9fd
denysthemenace <denys_alex6@hotmail.com>
2021-05-01 16:09:03 -0700

    updated some things in these files

--- client/src/components/dataTest.js
+++ client/src/components/dataTest.js
@@ -1,25 +1,28 @@
-const testData_1 = [
-    {"id":1,"ticker":"acd","name":"homelike","sector":"Industrials","date":"2021-04-21T15:18:14.447Z"},
-    {"id":2,"ticker":"acd","name":"homelike","sector":"Industrials","date":"2021-04-21T15:18:14.447Z"},
-    {"id":3,"ticker":"acd","name":"homelike","sector":"Industrials","date":"2021-04-21T15:18:14.447Z"},
-    {"id":4,"ticker":"","name":"turnsole","sector":"Industrials","date":"2021-04-21T15:18:14.446Z"},
-    {"id":5,"ticker":"","name":"turnsole","sector":"Industrials","date":"2021-04-21T15:18:14.446Z"},
-    {"id":6,"ticker":"","name":"turnsole","sector":"Industrials","date":"2021-04-21T15:18:14.446Z"},
-    {"id":7,"ticker":"","name":"turnsole","sector":"Industrials","date":"2021-04-21T15:18:14.446Z"},
-    {"id":8,"ticker":"","name":"uncoiled","sector":"Communication Services","date":"2021-04-21T15:18:14.443Z"},
-    {"id":9,"ticker":"","name":"proplyds","sector":"Real Estate","date":"2021-04-21T15:18:14.443Z"},
-    {"id":10,"ticker":"jml","name":"Saipan","sector":"Industrials","date":"2021-04-21T15:18:14.442Z"},
-    {"id":11,"ticker":"xjy","name":"ill-faurd","sector":"Real Estate","date":"2021-04-21T15:18:14.442Z"},
-    {"id":12,"ticker":"lzcb","name":"vantage","sector":"Financials","date":"2021-04-21T15:18:14.441Z"},
-]
+const testData_1 ={
+ "name": "TOPICS", "children": [{
+    "name": "Topic A",
+    "children": [
+        {"name": "Sub A1", "size": 4},
+        {"name": "Sub A2", "size": 4}]
+}, {
+    "name": "Topic B",
+    "children": [{"name": "Sub B1", "size": 3},
+     {"name": "Sub B2", "size": 3}, 
+     {"name": "Sub B3", "size": 3}]
+}, {
+    "name": "Topic C",
+    "children": [
+        {"name": "Sub C1", "size": 4},
+        {"name": "Sub C2", "size": 4}]
+}]
+}
 
-const testData_2 = [
-    {"id":1,"ticker":1,"name":1,"sector":1,"date":1},
-    {"id":2,"ticker":2,"name":2,"sector":2,"date":2},
-    {"id":3,"ticker":3,"name":3,"sector":3,"date":3},
-]
 
-const testData_3 = [{},{}]
+const testData_2 = {
+    "name": "TOPICS", "children": [{
+   }]
+}
+const testData_3 = {}
 
 const testData_4 = [{}]
 
