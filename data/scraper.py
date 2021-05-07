5b412aa5950e059e979d1ce3da95a0495f226801
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-05-07 15:08:31 -0700

    Update scraper.py

--- data/scraper.py
+++ data/scraper.py
@@ -10,7 +10,6 @@ from sqlalchemy import delete
 from datetime import datetime, timedelta
 from decouple import config
 
-
 def generateStock(ticker, name, sector):
     return Stock(
         ticker=ticker,
@@ -19,50 +18,69 @@ def generateStock(ticker, name, sector):
         date=datetime.now()
     )
 
-# Create Reddit Instance
-reddit = praw.Reddit(
-    user_agent=config('REDDIT_USER_AGENT'),
-    client_id=config('REDDIT_CLIENT_ID'),
-    client_secret=config('REDDIT_CLIENT_SECRET'),
-    username=config('REDDIT_USERNAME'),
-    password=config('REDDIT_PASSWORD'),
-)
-subreddit = reddit.subreddit("wallstreetbets")
+# global program flag
+program_flag = False
 
-# Parse XML File
-tree = ET.parse("StockListComplete.xml")
-root = tree.getroot()
+def run():
+    # turn on the program flag
+    global program_flag
+    program_flag = True
 
-Base = declarative_base()
-# recreate_database()  # Testing
-s = Session()
+    # program kill switch
+    def signal_handler(number, frame):
+        global program_flag
+        program_flag = False
 
-for comment in subreddit.stream.comments(skip_existing=True):
+    # kill signal handler
+    signal.signal(signal.SIGTERM, signal_handler)
 
-    cut_off = datetime.now() - timedelta(hours=24)
-    s.execute(delete(Stock).where(Stock.date < cut_off))
+    # Create Reddit Instance
+    reddit = praw.Reddit(
+        user_agent=config('REDDIT_USER_AGENT'),
+        client_id=config('REDDIT_CLIENT_ID'),
+        client_secret=config('REDDIT_CLIENT_SECRET'),
+        username=config('REDDIT_USERNAME'),
+        password=config('REDDIT_PASSWORD'),
+    )
+    subreddit = reddit.subreddit("wallstreetbets")
 
-    records = root.findall('record')
-    if len(records) > 0:
-        for record in root.findall('record'):
-            tickers = list(record.iter('StockTicker'))
-            fullnames = list(record.iter('StockFullname'))
-            sectors = list(record.iter('StockSector'))
-            if len(tickers) > 0:
-                ticker = tickers[0].text
-            else:
-                ticker = ''
-            if len(fullnames) > 0:
-                fullname = fullnames[0].text
-            else:
-                fullname = ''
-            if len(sectors) > 0:
-                sector = sectors[0].text
-            else:
-                sector = ''
-            if (ticker != '' and ticker in comment.body.split()) or (fullname != '' and fullname in comment.body):
-                new_stock = generateStock(ticker, fullname, sector)
-                s.add(new_stock)
+    # Parse XML File
+    tree = ET.parse("StockListComplete.xml")
+    root = tree.getroot()
 
-    s.commit()
-s.close()
+    Base = declarative_base()
+    # recreate_database()  # Testing
+    s = Session()
+
+    for comment in subreddit.stream.comments(skip_existing=True):
+
+        cut_off = datetime.now() - timedelta(hours=24)
+        s.execute(delete(Stock).where(Stock.date < cut_off))
+
+        records = root.findall('record')
+        if len(records) > 0:
+            for record in root.findall('record'):
+                tickers = list(record.iter('StockTicker'))
+                fullnames = list(record.iter('StockFullname'))
+                sectors = list(record.iter('StockSector'))
+                if len(tickers) > 0:
+                    ticker = tickers[0].text
+                else:
+                    ticker = ''
+                if len(fullnames) > 0:
+                    fullname = fullnames[0].text
+                else:
+                    fullname = ''
+                if len(sectors) > 0:
+                    sector = sectors[0].text
+                else:
+                    sector = ''
+                if (ticker != '' and ticker in comment.body.split()) or (fullname != '' and fullname in comment.body):
+                    new_stock = generateStock(ticker, fullname, sector)
+                    s.add(new_stock)
+
+        s.commit()
+    s.close()
+
+if __name__ == '__main__':
+    run()
